14a9a81fa26c685085db8e26170c03730cedc39c
Sikander RAVATE <sikander.ravate@gmail.com>
2026-06-18 15:58:02 +0200

    fix: update sha256 for final v0.4.0 aarch64-apple-darwin build

--- Formula/tincta-v2.rb
+++ Formula/tincta-v2.rb
@@ -7,7 +7,7 @@ class TinctaV2 < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/sindus/tincta-V2/releases/download/v0.4.0/tincta-v0.4.0-aarch64-apple-darwin.tar.gz"
-      sha256 "db258d1898734abeff8dc866f9f00b2783262d06104dd57fc6cd21f14bd46b08"
+      sha256 "27fc5919a114c10a5ee67bc5cfbf0e38fcc6e6a03a7c59d47eaa001c7bd92b13"
     end
   end
 
